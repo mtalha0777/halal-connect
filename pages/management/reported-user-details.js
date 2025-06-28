6397c8cf6229871bd61acc1e11909b5cd887ef94
Muhammad Talha <talhamaqsood25@gmail.com>
2025-06-28 10:36:37 +0500

    Update State Hook

--- pages/management/reported-user-details.js
+++ pages/management/reported-user-details.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState} from "react";
 import Image from "next/image";
 import Sidebar from "../../components/layout/SideBar";
 import Topbar from "../../components/layout/TopBar";

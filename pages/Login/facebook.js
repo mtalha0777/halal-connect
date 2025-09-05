1e7e70960df93f3ea9f72e20a56e6d4f3486fee1
mtalha0777 <talhamaqsood25@gmail.com>
2025-09-05 12:18:58 +0500

    Fixed Next.js route conflicts, removed duplicate pages

--- pages/Login/facebook.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function FacebookRedirect() {
-  useEffect(() => {
-    window.location.replace("https://www.facebook.com");
-  }, []);
-  return <p>Redirecting to Facebook…</p>; // optional loader message
-}
